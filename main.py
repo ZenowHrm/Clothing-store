16003dced69ea2141011e9e7bdd1ba5503c7b843
ZenowHrm <152890552+ZenowHrm@users.noreply.github.com>
2026-07-27 18:13:14 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import reflex as rx
+from fastapi import FastAPI
+
+from Backend.schemas.user import router as user
+
+backend_app = FastAPI()
+app = rx.App()
+
+backend_app.include_router(user, tags=["User"])
+app._api.mount("/api", backend_app)
+
+def index() -> rx.Component:
+    main = rx.el.div(
+        rx.el.h1(
+            "Hola mundo running"
+        ),
+        style= {
+            "background": "#09f"
+        }
+    )
+    
+    return main
+
+app.add_page(index, route="/")
