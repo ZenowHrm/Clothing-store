a5ea954be6c0486fa9a06fb57fef4f80ad880b2c
ZenowHrm <152890552+ZenowHrm@users.noreply.github.com>
2026-08-12 02:54:24 -0400

    Update rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -3,6 +3,7 @@ from reflex.plugins.sitemap import SitemapPlugin
 
 config = rx.Config(
     app_name="main",
+    api_url="https://clothing-store-y9pr.onrender.com",
     disable_plugins=[SitemapPlugin],
     plugins=[
         rx.plugins.RadixThemesPlugin()
