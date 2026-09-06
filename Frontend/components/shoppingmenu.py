a5f1b08da8abb02ee7c93120c3a68f99c8240fba
ZenowHrm <152890552+ZenowHrm@users.noreply.github.com>
2026-09-06 19:54:47 -0400

    actualizacion del link de mi portafolio

--- Frontend/components/shoppingmenu.py
+++ Frontend/components/shoppingmenu.py
@@ -135,7 +135,7 @@ def Shopping_cart():
                 rx.el.br(),
                 rx.el.br(),
                 "También puedes visitar mi ", 
-                rx.link("portafolio", href="https://portfoliosantimy.onrender.com"), 
+                rx.link("portafolio", href="https://portafolioreactsantimy.pages.dev/"), 
                 " para ver más cosas interesantes y contactarte conmigo directamente.",
             ),
             rx.button(
